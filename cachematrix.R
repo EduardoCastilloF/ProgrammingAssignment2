7aeedb6fb19c95dcf4f1464a450b62152997cda4
EduardoCastilloF <eduardoalfonsocastillo@gmail.com>
2020-10-24 23:54:21 +0100

    Update cachematrix.R
    Commiting my solution tothe assignment 2 Week 3 R Programming - Eduardo Castillo

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,28 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    i <- NULL
+
+    # Setting the matrix object and assigning it a value in a different environment
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    
+    # Get the matrix
+    get <- function() x
+    
+    # Setting the inverse and assigning it a value in a different environment
+    seti <- function(solve) i <<- solve
+    
+    # getting the inverse
+    geti <- function() i
+    
+    # Creates the object of the matrix
+    list(set = set, get = get,
+         seti = seti,
+         geti = geti)
+
 }
 
 
@@ -12,4 +34,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    print(x)
+    i <- x$geti()
+    
+    if(!is.null(i)) {
+      
+        message("getting cached data")
+        return(i)
+      
+    }
+    
+    data <- x$get()
+    
+    i <- solve(data, ...)
+    
+    x$seti(i)
+    
+    i
+    
 }
